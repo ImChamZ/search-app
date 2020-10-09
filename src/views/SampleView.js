0d1b67c2f7352c73bfff78c0a5e36f8feb6c3841
CC <chamdevin@gmail.com>
2020-10-09 15:46:48 +0530

    s test

--- src/views/SampleView.js
+++ src/views/SampleView.js
@@ -6,9 +6,7 @@ const SampleView = ({ increment, decrement, counter }) => {
       <h2>SampleView</h2>
       <label>Click Count : {counter}</label>
       <div>
-        <button onClick={() => increment()}>
-          Increment Button - Master Branch
-        </button>
+        <button onClick={() => increment()}>Increment Button</button>
         <button onClick={() => decrement()}>
           Decrement Button - Master Branch
         </button>
