faa2eaa49acd64e6c44f044b9fafead55da67833
CC <chamdevin@gmail.com>
2020-10-09 15:53:03 +0530

    s test

--- src/views/SampleView.js
+++ src/views/SampleView.js
@@ -3,7 +3,7 @@ import React from "react";
 const SampleView = ({ increment, decrement, counter }) => {
   return (
     <>
-      <h2>SampleView - Changed From Master</h2>
+      <h2>SampleView - Changed From Second Branch</h2>
       <label>Click Count : {counter}</label>
       <div>
         <button onClick={() => increment()}>Increment Button</button>
