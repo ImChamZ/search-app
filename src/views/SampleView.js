e6073ec961a28bf6aedc5d2a4095ce953974987c
CC <chamdevin@gmail.com>
2020-10-09 12:53:33 +0530

    b test

--- src/views/SampleView.js
+++ src/views/SampleView.js
@@ -7,7 +7,7 @@ const SampleView = ({ increment, decrement, counter }) => {
       <label>Click Count : {counter}</label>
       <div>
         <button onClick={() => increment()}>Increment Button</button>
-        <button onClick={() => decrement()}>Decrement Button</button>
+        <button onClick={() => decrement()}>Decrement</button>
       </div>
     </>
   );
